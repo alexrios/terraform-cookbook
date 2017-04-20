fd7671118ea61bdd83e3d254a24a03f1eef9fa44
Alex Rios <alex.rios@protonmail.com>
2017-04-19 21:32:15 -0300

    using tags

--- using-tags/main.tf
+++ using-tags/main.tf
@@ -7,4 +7,9 @@ resource "aws_instance" "example" {
   #Ubuntu 14.04 AMI (Amazon Machine Image)
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
+
+  tags {
+    #tags WILL CHANGE the state of previous runnings resources.
+    Name = "My Instance Name"
+  }
 }
